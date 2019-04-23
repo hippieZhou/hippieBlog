2a370eaa41290a5293a8ae09e54b7cdd9e2ad340
hippieZhou <HippieZhou@outlook.com>
2019-04-23 22:35:32 +0800

    update

--- app/music/views.py
+++ app/music/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
+from music_dl.extractors import qq
 from . import bp
 
 
 @bp.route('/')
 def index():
+    # https://github.com/0xHJK/music-dl
     return render_template('music/index.html', title='Music')
