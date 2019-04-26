bfb75823ea42a9729dac4555074260416b363d89
hippieZhou <zhouqiang@4dogs.cn>
2019-04-26 13:12:43 +0800

    update

--- app/api/api_bing.py
+++ app/api/api_bing.py
@@ -27,8 +27,28 @@ def bing():
                 return jsonify(code=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             print(e)
-            return jsonify(code=status.HTTP_500_INTERNAL_SERVER_ERROR, msg='day is error.')
+            return jsonify(code=status.HTTP_400_BAD_REQUEST, msg='day is error.')
     else:
         bings = Bing.query.all()
         return jsonify(code=status.HTTP_200_OK,
                        data=random.choice(bings).get_json())
+
+
+@bp.route('/bings', methods=['GET'])
+def bings():
+    headers = dict(request.headers)
+    token = headers.setdefault('Auth-Token', None)
+    content_type = headers.setdefault('Content-Type', None)
+    if token != 'hippiezhou.fun' or content_type is None or content_type != "application/json":
+        return jsonify(code=status.HTTP_403_FORBIDDEN, msg='not allowed.')
+
+    page = request.args.get('page', type=int)
+    per_page = request.args.get('per_page', type=int)
+
+    if page is None or per_page is None:
+        return jsonify(code=status.HTTP_400_BAD_REQUEST, msg='arguements is error.')
+    else:
+        from app.models import Bing
+        val = Bing.query.order_by(
+            Bing.datetime.desc()).paginate(page, per_page, False)
+        return jsonify(code=status.HTTP_200_OK, data=[p.get_json() for p in val.items])
