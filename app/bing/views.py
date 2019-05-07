c2eebee7e683babf69ab982dbd1cbfe3663b2438
hippieZhou <HippieZhou@outlook.com>
2019-05-07 21:32:24 +0800

    update

--- app/bing/views.py
+++ app/bing/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
-from . import bp
-from .bing_spider import restore_bing_wallpapers
 from datetime import date
+
+from . import bp
 
 
 @bp.route('/')
@@ -13,17 +13,18 @@ def index():
     today = date.today()
     print(today, first_time)
     if(first_time < today):
-        restore_bing_wallpapers()
+        from app.utils import bing_spider
+        bing_spider.restore_bing_wallpapers()
 
     bings = Bing.query.order_by(Bing.datetime.desc())
     page = request.args.get('page', 1, type=int)
     others = bings.paginate(page, 10, False)
 
-    return render_template('bing/index.html', others=others,title='Bing')
+    return render_template('bing/index.html', others=others, title='Bing')
 
 
 @bp.route('/detail/<hsh>', methods=['GET', 'POST'])
 def detail(hsh):
     from app.models import Bing
     first = Bing.query.filter_by(hsh=hsh).first_or_404()
-    return render_template('bing/detail.html', first=first,title='Bing')
+    return render_template('bing/detail.html', first=first, title='Bing')
