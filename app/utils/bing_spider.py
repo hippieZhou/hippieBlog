d7d4bf66a8a1605cdb3f33904dba157cb57b55e0
hippiezhou <hippiezhou@outlook.com>
2019-07-15 15:02:45 +0800

    update db and fixed some issues

--- app/utils/bing_spider.py
+++ app/utils/bing_spider.py
@@ -4,45 +4,15 @@ from app.models import Bing
 import requests
 import urllib
 import json
-import random
 
-USER_AGENTS = [
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
-    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
-    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
-    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
-    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
-    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"
-]
-
-COOKIES = "MUID=36117ED897B36D79118D73BE93B36B74; SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=C859143CC34E4A28AE303FE369686516&dmnchg=1; MUIDB=36117ED897B36D79118D73BE93B36B74; MSCC=1; ENSEARCH=BENVER=1; SerpPWA=reg=1; _TTSS_OUT=hist=['en']; ANON=A=8AC4A7383D0B3CA8FF579218FFFFFFFF&E=16d2&W=1; NAP=V=1.9&E=1678&C=WzjIRG8Fp5V1-yQ5RbtI5aEhS4nb_4wYqqQ-bk7vHwc4pRMD3GjxTA&W=1; _UR=MC=1; MSTC=ST=1; _tarLang=default=zh-Hans; ULC=P=115CF|141:@28&H=115CF|141:28&T=115CF|141:28:2; SRCHUSR=DOB=20190528&T=1562812375000; SRCHHPGUSR=CW=1286&CH=922&DPR=1&UTC=480&WTS=63698407785; _SS=SID=0E7F21996285671505CB2C0F63FA6698; _EDGE_S=SID=0E7F21996285671505CB2C0F63FA6698"
+USER_AGENG = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+COOKIES = "SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=550985DA3A594E778D50B0672C8B7B3C&dmnchg=1; _EDGE_V=1; MUID=1229F3F5A56B69751C77FE63A41468D4; MUIDB=1229F3F5A56B69751C77FE63A41468D4; ENSEARCH=BENVER=1; SerpPWA=reg=1; MSCC=1; MSTC=ST=1; _tarLang=default=zh-Hans; _EDGE_S=mkt=zh-cn&SID=2FDB4EA865B263983FA74332649C62ED; _FP=hta=on; ULC=P=1175F|7:3&H=1175F|7:3&T=1175F|7:3; ipv6=hit=1563175374895&t=4; SRCHHPGUSR=CW=1920&CH=947&DPR=1&UTC=480&WTS=63698768573; SRCHUSR=DOB=20190711&T=1563171781000; _SS=SID=2FDB4EA865B263983FA74332649C62ED&HV=1563171782&bIm=208"
 
 
 def spider():
     headers = {
         "Host": "cn.bing.com",
-        "User-Agent": random.choice(USER_AGENTS),
+        "User-Agent": USER_AGENG,
         'Cookie': COOKIES
     }
 
@@ -73,7 +43,7 @@ def spider():
                 model = Bing(hsh=hsh, pub_date=startdate, url=url,
                              urlbase=urlbase, copyright=copyright,
                              title=title, caption=caption, description=desc)
-                has = Bing.query.filter_by(pub_date=model.pub_date)
+                has = Bing.query.filter_by(pub_date=model.pub_date).first()
                 if has is None:
                     log.logger.debug(model.pub_date)
                     db.session.add(model)
